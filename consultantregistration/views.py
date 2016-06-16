ab6f51b9ba70e4ae14b573eaad9396f15457f780
dcklee <dcklee11@gmail.com>
2016-06-16 17:24:47 +0800

    Update Consultant Profile View to incorporate Enlingo SocialBOT Functionality

--- consultantregistration/views.py
+++ consultantregistration/views.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 from django.views import generic
 from consultantregistration.models import ConsultantRegistrationDetails, CourseConsultantRelationship
 from coursesearch.models import Course
+from pinax.blog.models import Post
 from django.contrib.auth.models import User
 from roles.models import Profile
 
@@ -48,4 +49,12 @@ class RegistrationView(generic.DetailView):
         context['workposition'] = self.object.consultantregistrationdetails.prior_work_position
         context['lived'] = self.object.consultantregistrationdetails.cities_lived_worked_in
         context['location'] = self.object.city
+        try:
+            context['enlingoposts'] = Post.objects.filter(author=self.object.user).order_by("published")
+            if context['enlingoposts']:
+                context['current_enlingopost'] = context['enlingoposts'][0]
+            else:
+                context['current_enlingopost'] = ''
+        except Post.DoesNotExist:
+            pass
         return context
